2c768fd717e424656474b3a52de0a1f606bfd353
Ituki Kirihara <ituki@fc.to>
2022-12-05 19:44:51 +0900

    migration実行後のスクリプトを、migration本体に移動
    ついでに、downでindex削除ミススを修正

--- src/migrations/20221204185000_alter_notes.js
+++ src/migrations/20221204185000_alter_notes.js
@@ -7,15 +7,28 @@ exports.up = function(knex, Promise) {
     return knex.raw('create index notes_idx_note_norm on notes using gin (note_norm gin_bigm_ops)');
   }).then(()=>{
     return knex.raw('drop index notes_idx_note');
+  }).then(async ()=>{
+    var rows = await knex('notes').select().orderBy('id')
+    for(var row of rows){
+      console.log('note: notes -> note_norm:' + row['id'])
+      await knex('notes')
+      .where({
+        id: row['id']
+      })
+      .update({
+        note_norm: row['note'].normalize('NFKC').toLowerCase()
+      })
+    }  
   })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.alterTable('notes', function(t) {
-    t.dropColumn('note_norm');       
+  return knex.raw('drop index notes_idx_note_norm')
+  .then(()=>{
+    return knex.schema.alterTable('notes', function(t) {
+    t.dropColumn('note_norm');
+    })
   }).then(()=>{
     return knex.raw('create index notes_idx_note on notes using gin (note gin_bigm_ops)');
-  }).then(()=>{
-    return knex.raw('drop index notes_idx_note_norm');
   });
 };

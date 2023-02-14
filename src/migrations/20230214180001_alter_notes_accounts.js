0a69929456fdc1f299a68efe6fe63795844cd0b9
Ituki Kirihara <ituki@fc.to>
2023-02-14 19:11:31 +0900

    検索結果の表示変更

--- src/migrations/20230214180001_alter_notes_accounts.js
+++ src/migrations/20230214180001_alter_notes_accounts.js
@@ -2,7 +2,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.alterTable('notes_accounts', function(t) {
-    t.text('domain_id');
+    t.bigint('domain_id');
     t.text('account_url');
   });
 };
--- src/routes/search.js
+++ src/routes/search.js
@@ -23,6 +23,23 @@ function escapeHTML( text )
     return text.replace( /["&'<>]/g, replacement );
 }
 
+function getRGB(colorcode,type){
+  if (!colorcode) return 0;
+  var cid = colorcode.split("")
+  cid.shift()
+  var c = "00"
+  if (type == 'r') c = cid[0] + cid[1]
+  if (type == 'g') c = cid[2] + cid[3]
+  if (type == 'b') c = cid[4] + cid[5]
+  return parseInt(c,16);
+}
+
+function urlLinker(text,domain){
+  text = text.replaceAll(/(https?\:\/\/[A-Za-z0-9\-\_\.]+[\u0021-\u007E]*)/g,'<a href="$1" target="_blank" rel="nofollow">$1</a>')
+  //text = text.replaceAll(/(\@[A-Za-z0-9]+)/g,'<a href="https://' + domain + '/$1" target="_blank" rel="nofollow">$1</a>')
+  return text;
+}
+
 //
 // searchページ
 router.get('/', function (req, res, next) {
@@ -36,7 +53,7 @@ router.get('/', function (req, res, next) {
     return;
   }
   var db = null;
-  if (sql.search_tag){
+  if (false && sql.search_tag){
     db = database.select(
       'notes_accounts.domain',
       'notes_accounts.username',
@@ -58,24 +75,24 @@ router.get('/', function (req, res, next) {
       'notes_accounts.domain',
       'notes_accounts.username',
       'notes_accounts.display_name',
-      'notes_accounts.url as account_url',
+      'notes_accounts.account_url',
       'notes_accounts.account_id',
-      //'notes_domains.domain_name',
-      //'notes_domains.icon_url',
-      //'notes_domains.background_color',
+      'notes_domains.domain_name',
+      'notes_domains.icon_url',
+      'notes_domains.background_color',
       'notes.url',
       'notes.note',
       'notes.note_created_at'
     ).from('notes')
     .join('notes_accounts','notes.account_id','=','notes_accounts.id')
-    //.join('notes_domains','notes_accounts.domain_id','=','notes_domains.id')
+    .join('notes_domains','notes_accounts.domain_id','=','notes_domains.id')
     .whereRaw(sql.sql,sql.params)
     .orderBy('notes.note_created_at', 'desc')
     .limit(100).offset(0)
   }
 
   db.then(rows=>{
-    res.render("search",{msg:"検索結果上位100件です",rows:rows,query:query,Moment:Moment,escapeHTML:escapeHTML})
+    res.render("search",{msg:"検索結果上位100件です",rows:rows,query:query,Moment:Moment,escapeHTML:escapeHTML,getRGB:getRGB,urlLinker:urlLinker})
   })
 });
 
